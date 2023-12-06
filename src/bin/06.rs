6229a5758ed368789508049d5be6062608742dab
nomnivore <6979410+nomnivore@users.noreply.github.com>
2023-12-06 15:30:33 -0500

    06

---
+++ src/bin/06.rs
@@ -0,0 +1,133 @@
+use std::{
+    iter::{zip, Zip},
+    vec::IntoIter,
+};
+
+use parsers::parse_input;
+
+advent_of_code::solution!(6);
+
+mod parsers {
+    use nom::{
+        branch::alt,
+        bytes::complete::tag,
+        character::complete::{digit1, line_ending, space0},
+        combinator::map,
+        multi::{fold_many1, many1},
+        sequence::{delimited, preceded, separated_pair},
+        IResult,
+    };
+
+    fn parse_u64(input: &str) -> IResult<&str, u64> {
+        digit1(input).map(|(input, value)| (input, value.parse().unwrap()))
+    }
+
+    fn nums(input: &str) -> IResult<&str, Vec<u64>> {
+        many1(delimited(space0, parse_u64, alt((space0, line_ending))))(input)
+    }
+
+    pub fn parse_input(input: &str) -> IResult<&str, (Vec<u64>, Vec<u64>)> {
+        separated_pair(
+            preceded(tag("Time:"), nums),
+            line_ending,
+            preceded(tag("Distance:"), nums),
+        )(input)
+    }
+
+    fn whole_num_u64(input: &str) -> IResult<&str, u64> {
+        map(
+            fold_many1(
+                delimited(space0, digit1, alt((space0, line_ending))),
+                String::new,
+                |mut acc, dig| {
+                    acc.push_str(dig);
+                    acc
+                },
+            ),
+            |s| s.parse().unwrap(),
+        )(input)
+    }
+
+    pub fn parse_input_2(input: &str) -> IResult<&str, (u64, u64)> {
+        separated_pair(
+            preceded(tag("Time:"), whole_num_u64),
+            line_ending,
+            preceded(tag("Distance:"), whole_num_u64),
+        )(input)
+    }
+}
+
+fn prepare(input: &str) -> Zip<IntoIter<u64>, IntoIter<u64>> {
+    let (_, (time, dist)) = parse_input(input).unwrap();
+
+    zip(time, dist)
+}
+
+#[allow(unused_variables)]
+#[allow(unused_must_use)]
+pub fn part_one(input: &str) -> Option<u64> {
+    let races = prepare(input);
+
+    // brute force approach
+    let result: usize = races
+        .map(|race| {
+            // convert race into # of ways to win
+            let (time, dist) = race;
+
+            // 0 & race.time are guaranteed losses
+
+            (1..time)
+                .filter(|hold| {
+                    let time_remaining = time - hold;
+
+                    hold * time_remaining > dist
+                })
+                .collect::<Vec<u64>>()
+                .len()
+        })
+        .product();
+
+    Some(result as u64)
+}
+
+#[allow(unused_variables)]
+#[allow(unused_must_use)]
+pub fn part_two(input: &str) -> Option<u64> {
+    let (_, (time, dist)) = parsers::parse_input_2(input).unwrap();
+
+    // again, brute force
+    // its slow but not as slow as yesterday. could optimize with binary search?
+
+    let result: u64 = (1..time)
+        .filter(|hold| {
+            let remaining = time - hold;
+
+            hold * remaining > dist
+        })
+        .collect::<Vec<u64>>()
+        .len()
+        .try_into()
+        .unwrap();
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "Time:      7  15   30
+Distance:  9  40  200";
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(EXAMPLE);
+        assert_eq!(result, Some(288));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(EXAMPLE);
+        assert_eq!(result, Some(71503));
+    }
+}
